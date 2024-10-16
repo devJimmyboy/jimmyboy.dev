1f1963d79d740cc917889abf53f6cc56798506f4
Jimmy Vrana <devJimmyboy@gmail.com>
2024-10-16 12:30:21 -0500

    update watermark

--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -1,20 +1,12 @@
-import * as React from "react"
-import Typography from "@mui/material/Typography"
-import MuiLink from "@mui/material/Link"
+import * as React from 'react'
+import Typography from '@mui/material/Typography'
+import MuiLink from '@mui/material/Link'
 
 export default function Copyright() {
   return (
-    <Typography
-      variant="body2"
-      width="max-content"
-      borderRadius="1em 1em 0 0"
-      ml={4}
-      p={2}
-      fontSize="1.25em"
-      color="text.secondary"
-      align="center">
-      {"Made by "}
-      <MuiLink fontWeight={700} color="inherit" href="https://jimmyboy.tv">
+    <Typography variant="body2" width="max-content" borderRadius="1em 1em 0 0" ml={4} p={2} fontSize="1.25em" color="text.secondary" align="center">
+      {'Made by '}
+      <MuiLink fontWeight={700} color="inherit" href="https://jimmyboy.dev">
         Jimmyboy
       </MuiLink>
     </Typography>
