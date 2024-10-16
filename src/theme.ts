b263bc00698a20befb6af4a8a6bd6522f970aff8
Jimmy Vrana <devJimmyboy@gmail.com>
2024-10-16 12:47:22 -0500

    start with vanilla vite mantine template

--- src/theme.ts
+++ src/theme.ts
@@ -1,50 +1,5 @@
-import { Roboto } from '@next/font/google'
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { createTheme } from "@mantine/core";
 
-// Create a theme instance.
-const theme = createTheme({
-  typography: {
-    fontFamily: '"DM Sans", sans-serif;',
-  },
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#f44336',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    peepo: {
-      main: 'rgba(50,150,75,0.9)',
-    },
-  },
-})
-
-export const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  fallback: ['Helvetica', 'Arial', 'sans-serif'],
-})
-
-declare module '@mui/material/styles' {
-  interface Palette {
-    peepo: Palette['primary']
-  }
-  interface PaletteOptions {
-    peepo: PaletteOptions['primary']
-  }
-
-  interface PaletteColor {
-    darker?: string
-  }
-  interface SimplePaletteColorOptions {
-    darker?: string
-  }
-}
-
-export default theme
+export const theme = createTheme({
+  /* Put your mantine theme override here */
+});
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["pages/**/*", "src/**/*", "components/**/*"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
